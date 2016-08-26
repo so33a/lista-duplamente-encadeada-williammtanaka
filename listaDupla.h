dcffe603862f74002854f1317871397b7fe61c9a
williammtanaka <william.tanakinha@hotmail.com>
2016-08-26 00:41:48 -0300

    Update listaDupla.h

--- listaDupla.h
+++ listaDupla.h
@@ -16,6 +16,7 @@ typedef struct {
 ListaDupla inicializa();
 link novoNo (int item, link prev, link next);
 link buscar(ListaDupla l, int item);
+link buscaNoMenorElemento (ListaDupla l);
 void insereDepois (ListaDupla l, link x, link t);
 void insereAntes (ListaDupla l, link x, link t);
 link removeNo (link x);
